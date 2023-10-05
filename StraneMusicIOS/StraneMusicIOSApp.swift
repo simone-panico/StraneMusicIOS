b7ebd922dbe226c81c97d675e3654f2f1495686d
Simone <simone.panico@icloud.com>
2023-10-05 21:16:05 +0200

    Initial Commit

---
+++ StraneMusicIOS/StraneMusicIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StraneMusicIOSApp.swift
+//  StraneMusicIOS
+//
+//  Created by Simone Panico on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StraneMusicIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
