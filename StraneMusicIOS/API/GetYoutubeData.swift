f87e838837aa18157a6a70f07310be7a03ca26cc
Simone <simone.panico@icloud.com>
2023-10-20 23:50:35 +0200

    YT API

--- StraneMusicIOS/API/GetYoutubeData.swift
+++ StraneMusicIOS/API/GetYoutubeData.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class getData {
+    func get_request() {
+        let url = URL(string: "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=starboy&key=AIzaSyAS2U9183D5RlbCc3C9E9mzfPFPS4-XqQg")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                    error == nil,
+                    let data = data,
+                    let string = String(data: data, encoding: .utf8)
+                
+
+                print(string)
+            }
+            task.resume()
+        
+    }
+}
+
