264267ac1db59067828a1107817e670cf5a1e49b
Simone Panico <123191545+simone-panico@users.noreply.github.com>
2024-07-06 16:13:26 +0200

    Update GetYoutubeData.swift

--- StraneMusicIOS/API/GetYoutubeData.swift
+++ StraneMusicIOS/API/GetYoutubeData.swift
@@ -1,26 +1,3 @@
-/*import Foundation
-
-get_request()
-    func get_request() {
-        print("HEllo2")
-        let url = URL(string: "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=starboy&key=AIzaSyAS2U9183D5RlbCc3C9E9mzfPFPS4-XqQg")!
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                guard
-                    error == nil,
-                    let data = data,
-                    let string = String(data: data, encoding: .utf8)
-                else {
-                    print(error ?? "Unknown error")
-                    return
-                }
-
-                print(string)
-            }
-            task.resume()
-}
-
-*/
-
 import Foundation
 
 // Definiere eine Struktur, die die JSON-Daten repräsentiert
